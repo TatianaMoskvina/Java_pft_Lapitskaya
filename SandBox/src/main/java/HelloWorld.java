9a1b84a992fd06a8ea2d53f27b640f4899f478c1
Tatiana Moskvina <moskvina.tania.vit@gmail.com>
2020-09-22 12:54:43 +0700

    Project "SandBox" was added

---
+++ SandBox/src/main/java/HelloWorld.java
@@ -0,0 +1,18 @@
+{\rtf1\ansi\ansicpg1251\cocoartf1561\cocoasubrtf600
+{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red13\green0\blue129;\red239\green242\blue247;\red36\green38\blue41;
+\red37\green127\blue159;\red104\green26\blue29;\red43\green39\blue19;}
+{\*\expandedcolortbl;;\cssrgb\c6275\c6275\c58039;\cssrgb\c94902\c96078\c97647;\cssrgb\c18824\c20000\c21176;
+\cssrgb\c16863\c56863\c68627;\cssrgb\c49020\c15294\c15294;\cssrgb\c22353\c20000\c9412;}
+{\info
+{\keywords cursorLocation=1902, fitsPagesWidth=1}}\paperw11900\paperh16840\margl1008\margr1008\margt1008\vieww11360\viewh11700\viewkind1\viewscale81
+\deftab720
+\pard\pardeftab720\sl320\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 public\cf4 \strokec4  \cf2 \strokec2 class\cf4 \strokec4  \cf5 \strokec5 HelloWorld\cf4 \strokec4  \{\
+    \cf2 \strokec2 public\cf4 \strokec4  \cf2 \strokec2 static\cf4 \strokec4  \cf2 \strokec2 void\cf4 \strokec4  main(\cf5 \strokec5 String\cf4 \strokec4  args[]) \{\
+        \cf5 \strokec5 System\cf4 \strokec4 .\cf2 \strokec2 out\cf4 \strokec4 .println(\cf6 \strokec6 "Hello World!"\cf4 \strokec4 );\
+    \}\
+\}\cf7 \strokec7 \
+}
